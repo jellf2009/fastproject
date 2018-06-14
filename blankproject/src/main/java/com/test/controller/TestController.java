dcc70d0abf7424446a0a881dd36c0098277fb3d8
jellf2009 <jellf2009@163.com>
2018-06-14 19:20:22 +0800

    logback OK

--- blankproject/src/main/java/com/test/controller/TestController.java
+++ blankproject/src/main/java/com/test/controller/TestController.java
@@ -3,6 +3,8 @@ package com.test.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.test.pojo.User;
 import com.test.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class TestController {
 
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -24,8 +27,10 @@ public class TestController {
     @RequestMapping(value = "/test", produces = "text/json;charset=utf-8") //produces 可以保证输出的json数据的编码格式
     @ResponseBody
     public String showTest(HttpServletRequest request) {
+        logger.info("test test");
         User user = testService.findUserById(1L);
         String result = JSONObject.toJSONString(user);
+        logger.info("查询得到的用户信息 : " + result);
         return result;
     }
 
